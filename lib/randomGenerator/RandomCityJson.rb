c2e72fcfd77f35d82eecf41444c1981919f702c1
Tech-A06 <hibah28@live.co.uk>
2019-02-01 10:05:33 +0000

    it can read the json file and prints out a random city from the json.

--- lib/randomGenerator/RandomCityJson.rb
+++ lib/randomGenerator/RandomCityJson.rb
@@ -3,22 +3,20 @@ require 'json'
 class RandomCityJson
   # attr_accessor :json_length
 
-  def read_file
+  def initialize
     @json_data = JSON.parse(File.read('city_list.json'))
   end
 
+  def array_length
+    @json_data.length
+  end
+  
   def generate_random_cities(city_number)
-    random_city = @json_data[city_number]
+    @json_data[city_number]
   end
 
-  def retrieve_json_length
-    json_length = @json_data.length
+  def retrieve_id
+    @json_data[]
   end
 
-  def print_data
-   @json_data
-  end
 end
-
-test = RandomCityJson.new
-p test.print_data
--- spec/single_city_spec.rb
+++ spec/single_city_spec.rb
@@ -3,12 +3,16 @@
  describe 'Open Weather API ' do
  
     before(:all) do
+    @json_data = Generator.new.city
+    p @random_city_info = Generator.new.city.generate_random_cities(rand(0..(@json_data.array_length)))
 
-    @json_length = Generator.new.city.print_data
   #    @random_json_city  = Generator.new.cities.generate_random_cities(rand(0..@json_length))
-    @json_length.class 
-        
-
+    end
+    it 'print text' do 
+    
+    end
+  
 end
 
-end
+
+# git blame 
